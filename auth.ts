f7b0c5dcbfc314af6938846927a1704d3d49ffd7
prakhar-D01 <prakhardubey110@gmail.com>
2026-07-02 19:30:12 +0530

    03- Authentication

---
+++ auth.ts
@@ -0,0 +1,106 @@
+import NextAuth from "next-auth";
+import { PrismaAdapter } from "@auth/prisma-adapter";
+import authConfig from "./auth.config";
+import { db } from "./lib/db";
+import { getUserById, getAccountByUserId } from "./modules/auth/actions/index";
+
+export const { handlers, signIn, signOut, auth } = NextAuth({
+  session: {
+    strategy: "jwt",
+  },
+  callbacks: {
+    async signIn({ user, account }) {
+      if (!user || !account || !user.email) return false;
+
+      const existingUser = await db.user.findUnique({
+        where: { email: user.email },
+      });
+      if (!existingUser) {
+        const newUser = await db.user.create({
+          data: {
+            email: user.email!,
+            name: user.name,
+            image: user.image,
+
+            accounts: {
+              // @ts-ignore
+              create: {
+                type: account.type,
+                provider: account.provider,
+                providerAccountId: account.providerAccountId,
+                refreshToken: account.refresh_token,
+                accessToken: account.access_token,
+                expiresAt: account.expires_at,
+                tokenType: account.token_type,
+                scope: account.scope,
+                idToken: account.id_token,
+                sessionState: account.session_state,
+              },
+            },
+          },
+        });
+        if (!newUser) return false;
+      } else {
+        // Link the account if user exists
+        const existingAccount = await db.account.findUnique({
+          where: {
+            provider_providerAccountId: {
+              provider: account.provider,
+              providerAccountId: account.providerAccountId,
+            },
+          },
+        });
+
+        // If the account does not exist, create it
+        if (!existingAccount) {
+          await db.account.create({
+            data: {
+              userId: existingUser.id,
+              type: account.type,
+              provider: account.provider,
+              providerAccountId: account.providerAccountId,
+              refreshToken: account.refresh_token,
+              accessToken: account.access_token,
+              expiresAt: account.expires_at,
+              tokenType: account.token_type,
+              scope: account.scope,
+              idToken: account.id_token,
+              // @ts-ignore
+              sessionState: account.session_state,
+            },
+          });
+        }
+      }
+      return true;
+    },
+    async jwt({ token }) {
+      if (!token.sub) return token;
+      const existingUser = await getUserById(token.sub);
+
+      if (!existingUser) return token;
+
+      const exisitingAccount = await getAccountByUserId(existingUser.id);
+
+      token.name = existingUser.name;
+      token.email = existingUser.email;
+      token.role = existingUser.role;
+
+      return token;
+    },
+    async session({ session, token }) {
+      // Attach the user ID from the token to the session
+      if (token.sub && session.user) {
+        session.user.id = token.sub;
+      }
+
+      if (token.role && session.user) {
+        session.user.role = token.role;
+      }
+
+      return session;
+    },
+  },
+  secret: process.env.AUTH_SECRET,
+  adapter: PrismaAdapter(db),
+  ...authConfig,
+});
